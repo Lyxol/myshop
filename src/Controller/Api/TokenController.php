9c3a037e14558820416d21446ad5b8c87d739390
Lyxol <corbioli.lilian@gmail.com>
2024-01-09 11:35:17 +0100

    feature: add register method and finalize login method

--- src/Controller/Api/TokenController.php
+++ src/Controller/Api/TokenController.php
@@ -2,18 +2,23 @@
 
 namespace App\Controller\Api;
 
+use App\Entity\Account;
 use App\Entity\Token;
 use App\Repository\AccountRepository;
+use App\Repository\TokenRepository;
+use DateTime;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\String\ByteString;
 
 class TokenController extends AbstractController
 {
     public function __construct(
         private AccountRepository $accountRepository,
+        private TokenRepository $tokenRepository,
         private EntityManagerInterface $entityManager
     ) {}
 
@@ -23,14 +28,59 @@ class TokenController extends AbstractController
         $body = json_decode($request->getContent());
         if ($body === null || !isset($body->email) || !isset($body->password))
             return $this->json(["error" => "Incorrect Request Body"],400);
+
         $email = $body->email;
         $password = $body->password;
         $account = $this->accountRepository->findByEmailAndPassword($email, $password);
         if($account === null)
             return $this->json(["error" => "No Account Were Found"],401);
+
+        //TODO:à tester
+        $token =  $this->tokenRepository->findOneByAccount($account);
+        if($token !== null)
+            $this->entityManager->remove($token);
         $token = new Token();
+        do {
+            $tkn_string = ByteString::fromRandom(255)->toString();
+        } while ($this->tokenRepository->findOneByToken($tkn_string) !== null);
+        $token->setToken($tkn_string);
         $token->setAccount($account);
-        //TODO:Add token creation logic
-        return $this->json(["received" => true]);
+        $token->setExpireAt((new DateTime())->modify('+1 hour'));
+        $this->entityManager->persist($token);
+        $this->entityManager->flush();
+
+        return $this->json(["token" => $token->getToken()]);
+    }
+
+    #[Route('/api/register', name: 'app_api_register')]
+    public function register(Request $request): Response
+    {
+        $body = json_decode($request->getContent());
+        if ($body === null || !isset($body->email) || !isset($body->password))
+            return $this->json(["error" => "Incorrect Request Body"],400);
+
+        $email = $body->email;
+        $password = $body->password;
+        $account = $this->accountRepository->findByEmailAndPassword($email, $password);
+        if($account !== null)
+            return $this->json(["error" => "This account already exist"],401);
+
+        $account = new Account();
+        $account->setEmail($email);
+        $account->setPassword($password);
+        $this->entityManager->persist($account);
+        $this->entityManager->flush();
+
+        $token = new Token();
+        do {
+            $tkn_string = ByteString::fromRandom(255)->toString();
+        } while ($this->tokenRepository->findOneByToken($tkn_string) !== null);
+        $token->setToken($tkn_string);
+        $token->setAccount($account);
+        $token->setExpireAt((new DateTime())->modify('+1 hour'));
+        $this->entityManager->persist($token);
+        $this->entityManager->flush();
+        
+        return $this->json(["token" => $token->getToken()]);
     }
 }
