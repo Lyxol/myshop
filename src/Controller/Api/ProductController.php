85440531e187189b878b9b0184dfc97d71f15111
Lyxol <corbioli.lilian@gmail.com>
2024-02-01 14:33:22 +0100

    update: update ProductController & UserController

--- src/Controller/Api/ProductController.php
+++ src/Controller/Api/ProductController.php
@@ -11,6 +11,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpKernel\Attribute\MapQueryParameter;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Attribute\IsGranted;
+use Symfony\Component\Serializer\Normalizer\AbstractNormalizer;
 use Symfony\Component\Serializer\SerializerInterface;
 
 class ProductController extends AbstractController
@@ -36,37 +37,83 @@ class ProductController extends AbstractController
     }
 
     #[Route('/api/product/{id}', name: 'app_api_one_product', methods: ['GET'])]
-    public function getOneProduct(int $id) : JsonResponse
+    public function getOneProduct(Product $product = null): JsonResponse
     {
-        if ($this->productRepository->findOneById($id) === null)
-            return $this->json(["error" => "This product doesn't exist"], 404);
-        return $this->json($this->productRepository->findOneById($id));
+        if ($product === null)
+            return $this->json([
+                "code" => JsonResponse::HTTP_NOT_FOUND,
+                "message" => "This product does not exist"
+            ], JsonResponse::HTTP_NOT_FOUND);
+        return $this->json($product);
     }
     //<
     //>Need Admin role
     #[Route('/api/product', name: "app_api_create_product", methods: ['POST'])]
     #[IsGranted('ROLE_ADMIN', message: "Unauthorized")]
-    public function createProduct(Request $request, SerializerInterface $serializer) : JsonResponse
+    public function createProduct(Request $request, SerializerInterface $serializer): JsonResponse
     {
-        $product = $serializer->deserialize($request->getContent(), Product::class, 'json');
+        try {
+            $product = $serializer->deserialize($request->getContent(), Product::class, 'json');
+        } catch (\Throwable $th) {
+            return $this->json([
+                "code" => JsonResponse::HTTP_NOT_ACCEPTABLE,
+                "message" => $th->getMessage()
+            ], JsonResponse::HTTP_NOT_ACCEPTABLE);
+        }
         if ($this->productRepository->findOneByName($product->getName()) !== null)
-            return $this->json(["error" => "this product already exist"], 409);
+            return $this->json([
+                "code" => JsonResponse::HTTP_CONFLICT,
+                "message" => "this product already exist"
+            ], JsonResponse::HTTP_CONFLICT);
         $this->entityManagerInterface->persist($product);
+        $this->entityManagerInterface->flush();
+        return $this->json($product);
+    }
+
+    #[Route('/api/product/{id}', name: "app_api_update_product", methods: ['PUT'])]
+    #[IsGranted('ROLE_ADMIN', message: "Unauthorized")]
+    public function updateProduct(Product $product = null, Request $request, SerializerInterface $serializer)
+    {
+        if ($product === null) return $this->json([
+            "code" => JsonResponse::HTTP_NOT_FOUND,
+            "message" => "product not found"
+        ], JsonResponse::HTTP_NOT_FOUND);
+
+        if (empty($request->getContent())) return $this->json([
+            "code" => JsonResponse::HTTP_NOT_FOUND,
+            "message" => "this request has no content"
+        ], JsonResponse::HTTP_NOT_FOUND);
+
+        try {
+            $updatedProduct = $serializer->deserialize(
+                $request->getContent(),
+                Product::class,
+                'json',
+                [AbstractNormalizer::OBJECT_TO_POPULATE => $product]
+            );
+        } catch (\Throwable $th) {
+            return $this->json([
+                "code" => JsonResponse::HTTP_NOT_ACCEPTABLE,
+                "message" => $th->getMessage()
+            ], JsonResponse::HTTP_NOT_ACCEPTABLE);
+        }
+        $this->entityManagerInterface->persist($updatedProduct);
         $this->entityManagerInterface->flush();
         return $this->json($product);
     }
 
     #[Route('/api/product/{id}', name: "app_api_delete_product", methods: ['DELETE'])]
     #[IsGranted('ROLE_ADMIN', message: "Unauthorized")]
-    public function deleteProduct(int $id) : JsonResponse
+    public function deleteProduct(Product $product = null): JsonResponse
     {
-
-        $product = $this->productRepository->findOneById($id);
         if ($product === null)
-            return $this->json(["error" => "product not found"], 404);
+            return $this->json([
+                "code" => JsonResponse::HTTP_NOT_FOUND,
+                "message" => "product not found"
+            ], JsonResponse::HTTP_NOT_FOUND);
         $this->entityManagerInterface->remove($product);
         $this->entityManagerInterface->flush();
-        return $this->json($product);
+        return $this->json(null, JsonResponse::HTTP_NO_CONTENT);
     }
     //<
 }
--- src/Controller/Api/UserController.php
+++ src/Controller/Api/UserController.php
@@ -8,8 +8,10 @@ use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Attribute\IsGranted;
+use Symfony\Component\Serializer\Normalizer\AbstractNormalizer;
 use Symfony\Component\Serializer\SerializerInterface;
 
 class UserController extends AbstractController
@@ -17,7 +19,8 @@ class UserController extends AbstractController
 
     public function __construct(
         private AccountRepository $accountRepository,
-        private EntityManagerInterface $entityManager
+        private EntityManagerInterface $entityManager,
+        private UserPasswordHasherInterface $userPasswordHasherInterface
     ) {
     }
 
@@ -31,33 +34,96 @@ class UserController extends AbstractController
 
     #[IsGranted("ROLE_ADMIN", message: "Unauthorized")]
     #[Route('/api/user/{id}', name: 'app_api_show_one_account', methods: ['GET'])]
-    public function showOneAccount(int $id): JsonResponse
+    public function showOneAccount(Account $account = null): JsonResponse
     {
-        $user = $this->accountRepository->findOneById($id);
-        if ($user === null)
-            return $this->json(["error" => "account not found"]);
-        return $this->json($user);
+        if ($account === null)
+            return $this->json(
+                [
+                    "code" => JsonResponse::HTTP_NOT_FOUND,
+                    "message" => "account not found"
+                ],
+                JsonResponse::HTTP_NOT_FOUND
+            );
+        return $this->json($account);
     }
 
-    //TODO: à tester
+    #[IsGranted("ROLE_ADMIN", message: "Unauthorized")]
+    #[Route('/api/user/{id}', name: 'app_api_update_account', methods: ['PUT'])]
+    public function updateAccount(Account $account = null, Request $request, SerializerInterface $serializer)
+    {
+        if ($account === null) return $this->json([
+            "code" => JsonResponse::HTTP_NOT_FOUND,
+            "message" => "account not found"
+        ], JsonResponse::HTTP_NOT_FOUND);
+
+        if (empty($request->getContent())) return $this->json([
+            "code" => JsonResponse::HTTP_NOT_FOUND,
+            "message" => "this request has no content"
+        ], JsonResponse::HTTP_NOT_FOUND);
+
+        try {
+            $updatedAccount = $serializer->deserialize(
+                $request->getContent(),
+                Account::class,
+                'json',
+                [AbstractNormalizer::OBJECT_TO_POPULATE => $account]
+            );
+            $updatedAccount->setPassword(
+                $this->userPasswordHasherInterface->hashPassword($updatedAccount,$updatedAccount->getPassword())
+            );
+        } catch (\Throwable $th) {
+            return $this->json([
+                "code" => JsonResponse::HTTP_NOT_ACCEPTABLE,
+                "message" => $th->getMessage()
+            ], JsonResponse::HTTP_NOT_ACCEPTABLE);
+        }
+
+        $this->entityManager->persist($updatedAccount);
+        $this->entityManager->flush();
+        return $this->json($account);
+    }
+
     #[IsGranted("ROLE_ADMIN", message: "Unauthorized")]
     #[Route('/api/user', name: 'app_api_create_account', methods: ['POST'])]
     public function createAccount(Request $request, SerializerInterface $serializer)
     {
-        $account = $serializer->deserialize($request->getContent(), Account::class, 'json');
+        try {
+            $account = $serializer->deserialize($request->getContent(), Account::class, 'json');
+        } catch (\Throwable $th) {
+            return $this->json([
+                "code" => JsonResponse::HTTP_NOT_ACCEPTABLE,
+                "message" => $th->getMessage()
+            ], JsonResponse::HTTP_NOT_ACCEPTABLE);
+        }
+        $account->setPassword(
+            $this->userPasswordHasherInterface->hashPassword($account, $account->getPassword())
+        );
         if ($this->accountRepository->findOneByEmail($account->getEmail()) !== null)
-            return $this->json(["error" => "already exist"], 409);
+            return $this->json([
+                "code" => JsonResponse::HTTP_CONFLICT,
+                "message" => "already exist"
+            ], JsonResponse::HTTP_CONFLICT);
+        $this->entityManager->persist($account);
+        $this->entityManager->flush();
         return $this->json($account);
     }
 
     #[IsGranted("ROLE_ADMIN", message: "Unauthorized")]
     #[Route('/api/user/{id}', name: 'app_api_delete_account', methods: ['DELETE'])]
-    public function deleteAccount(int $id)
+    public function deleteAccount(Account $account)
     {
-        $account = $this->accountRepository->findOneById($id);
-        if ($account === null) return $this->json(["error" => "account not found"], 404);
+        if ($account->getId() === $this->getUser()->getId()) return $this->json([
+            "code" => JsonResponse::HTTP_UNAUTHORIZED,
+            "message" => "cannot delete yourself"
+        ], JsonResponse::HTTP_UNAUTHORIZED);
+
+        if ($account === null) return $this->json([
+            "code" => JsonResponse::HTTP_NOT_FOUND,
+            "message" => "account not found"
+        ], JsonResponse::HTTP_NOT_FOUND);
+
         $this->entityManager->remove($account);
         $this->entityManager->flush();
-        return $this->json($account);
+        return $this->json(null, JsonResponse::HTTP_NO_CONTENT);
     }
 }
